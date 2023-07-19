c96ab430fd5b1036920463038b9e43ffbc8b7591
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-19 19:38:12 +0300

    not done

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Links from "../components/Links"
 
-function About(props) {
+function About({bio , links}) {
 //issue is If a prop of bio is not passed down, 
 //or if bio is an empty string, don't display the <p> tag
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {bio && bio.length > 1 ? <p>{ bio }</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={links.github} linkedin={links.linkedin}/>
     </div>
   );
 }
