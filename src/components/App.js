b7047c7c10c1b48a44dcbf7e4ade9e9a1a8ad113
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-20 09:43:57 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links"
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -13,8 +12,7 @@ function App() {
     <div>
       <NavBar />
       <Home color={user.color} name={user.name} city={user.city}/>
-      <About bio={user.bio} links={user.links}/>
-      
+      <About bio={user.bio} links={user.links}/> 
     </div>
   );
 }
